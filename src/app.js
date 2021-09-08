b7e6cdf306371676a8d5ea83b431c748fda06e76
uesleipedro <ielseu@hotmail.com>
2021-09-07 23:16:55 -0300

    Solve cors problems

--- src/app.js
+++ src/app.js
@@ -3,7 +3,22 @@ const app = express();
 
 const scraping = require('./app/services/scraping');
 
+app.get('/', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    //res.send('cors problem fixed:)');
+});
+
 app.get('/articles/:page', async (req, res, next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
     async function runAsync() {
         const page = req.params.page;
         var data = await scraping.scrape(page);
@@ -13,6 +28,12 @@ app.get('/articles/:page', async (req, res, next) => {
 });
 
 app.get('/externalArticles/:page', async (req, res, next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
     async function runAsync() {
         const page = req.params.page;
         var data = await scraping.newsnow(page);
