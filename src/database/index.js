c774483414d00ca36161116add1ea468fdedc523
uesleipedro <ielseu@hotmail.com>
2021-12-05 15:19:51 -0300

    add vercel.json config alteration2

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 function connectToDatabase() {
     mongoose.connect(
-        process.env.DATABASE_URL,
+        "mongodb+srv://ueslei:angra136@cluster0.p5iuj.mongodb.net/spacetoday?retryWrites=true&w=majority",
+        // process.env.DATABASE_URL,
         //'mongodb://localhost:27017/spacetoday',
         {
             useNewUrlParser: true,
